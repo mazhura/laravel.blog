0439eb8e116c969c5e84f3f29523871625bf8dc0
Mykhailo Mazhura <mazhura1@gmail.com>
2021-03-24 14:04:45 +0200

    - changed README.md

--- resources/views/admin/layouts/layout.blade.php
+++ resources/views/admin/layouts/layout.blade.php
@@ -299,11 +299,11 @@
 
     $('.select2').select2()
 
-    //Initialize Select2 Elements
+    /*//Initialize Select2 Elements
     $('.select2bs4').select2({
         theme: 'bootstrap4'
     })
-
+*/
 
 
 </script>
